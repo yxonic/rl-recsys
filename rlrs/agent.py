622595a443f15937fe92b7711fd7b7b7cd2a192d
huangzhenya <huang3644129@163.com>
2018-12-22 19:53:40 -0600

    agent.py: update DQN in agent.py

--- rlrs/agent.py
+++ rlrs/agent.py
@@ -1,19 +1,38 @@
 import fret
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 from .dataprep import load_question, load_knowledge, load_embedding
 from .environment import QuesNet
 
 # to be update about N_actions and N_states
-N_STATES = 10
-N_ACTIONS = 5
+N_STATES = 100
+N_ACTIONS = 50
 
 @fret.configurable
 class DQN:
     def __init__(self,
                  double_q=False,
+                 memory_capacity=(2000, 'max saved memory states'),
+                 learning_rate=(0.001, 'learning rate'),
+                 greedy_epsilon=(0.9, 'greedy policy'),
+                 gama=(0.9, 'reward discount rate')
                  ):
+        self.learn_step_counter = 0
+        self.memory_counter = 0
+        self.memory = np.zeros(memory_capacity, )
+        self.learning_rate = learning_rate
+        self.greedy_epsilon = greedy_epsilon
+        self.gama = gama
+
+        # build DQN network
+        self.current_net, self.target_net = Net(), Net()
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=learning_rate)
+        self.loss_func = nn.MSELoss()
+
+    def select_action(self, observation):
+
         pass
 
 
@@ -25,7 +44,13 @@ class Net(nn.Module):
                  ):
         super(Net, self).__init__()
 
-        self.input_net = nn.Linear(N_STATES, 50)
+        self.input_net = nn.Linear(N_STATES, 200)
         self.input_net.weight.data.normal_(0, 0.1)
-        self.out_net = nn.Linear(50, N_ACTIONS)
+        self.out_net = nn.Linear(200, N_ACTIONS)
         self.out_net.weight.data.normal_(0, 0.1)
+
+    def forward(self, x):
+        x = F.relu(self.input_net(x))
+        out = self.out_net(x)
+        return out
+
