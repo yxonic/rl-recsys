8d549898fd7ad5866cd2e3841b2016131bcefffe
huangzhenya <huang3644129@163.com>
2018-12-24 16:59:45 -0600

    agent.py: update DQN: select_action(), store_transition()

--- rlrs/agent.py
+++ rlrs/agent.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from .sp_models import QuesNet
+from .dataprep import Questions
 
 # to be update about N_actions and N_states
 N_STATES = 100
@@ -14,49 +15,73 @@ TARGET_REPLACE_ITER = 100   # target update frequency
 @fret.configurable
 class DQN:
     def __init__(self,
-                 double_q=False,
+                 dataset=('zhixue', 'student record dataset',
+                          ['zhixue', 'poj', 'ustcoj']),
                  memory_capacity=(2000, 'max saved memory states'),
                  learning_rate=(0.001, 'learning rate'),
                  greedy_epsilon=(0.9, 'greedy policy'),
-                 gama=(0.9, 'reward discount rate')
+                 gama=(0.9, 'reward discount rate'),
+                 double_q = False,
                  ):
         self.learn_step_counter = 0
-        self.memory_counter = 0
-        self.memory_capacity = memory_capacity
-        self.memory = np.zeros(memory_capacity, )
         self.learning_rate = learning_rate
         self.greedy_epsilon = greedy_epsilon
         self.gama = gama
 
+        self.dataset = dataset
+        self._questions = Questions(dataset)
+        self.n_questions = self._questions.n_questions
+
+        self.memory_counter = 0
+        self.memory_capacity = memory_capacity
+        self.memory = np.zeros(memory_capacity, self.n_questions * 2 + 2)
+
+        self._rec_history = []
+        self._pred_scores = []
+
         # build DQN network
-        self.current_net, self.target_net = Net(), Net()
-        self.optimizer = torch.optim.Adam(self.current_net.parameters(),
-                                          lr=learning_rate)
-        self.loss_func = nn.MSELoss()
+        self.current_net, self.target_net = SimpleNet(), SimpleNet()
+
+        # self.optimizer = torch.optim.Adam(self.current_net.parameters(), lr=learning_rate)
+        # self.loss_func = nn.MSELoss()
 
     def select_action(self, observation):
         # to be updated of input observation
-        question, score = observation
-        x = torch.unsqueeze(torch.FloatTensor(observation), 0)
+        [p_score] = observation
+        self._pred_scores.append(p_score)
+
+        # generate
+        _action = self._rec_history[-1]
+        feature = self.generate_state_feature(_action, p_score)
+        feature = torch.unsqueeze(torch.FloatTensor(feature), 0)
+
         if np.random.uniform() < self.greedy_epsilon:
-            action_values = self.current_net(x)
+            action_values = self.current_net(feature)
             action = torch.max(action_values, 1)[1].data.numpy()
         else:
-            action = np.random.randint(0, N_ACTIONS)
+            action = np.random.randint(0, self.n_questions)
 
+        self._rec_history.append(action)
         return action
 
     # to be updated, 需要设定到几步之后才train的嘛
     def store_transition(self, state, action, reward, state_):
-        pass
+        transition = np.hstack((state, action, reward, state_))
+        index = self.memory_counter % self.memory_capacity
+        self.memory[index:] = transition
+        self.memory_counter += 1
 
+    # to be updated
     def learn(self):
         if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
             self.target_net.load_state_dict(self.current_net.state_dict())
+        self.learn_step_counter += 1
 
         # to be updated, training on batch ?
         sample_index = np.random.choice(self.memory_capacity, self.BATCH_SIZE)
         b_memory = self.memory[sample_index, :]
+        b_s = torch.FloatTensor(b_memory[:, :1])
+
         b_s = torch.FloatTensor(b_memory[:, :N_STATES])
         b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES + 1].astype(int))
         b_r = torch.FloatTensor(b_memory[:, N_STATES + 1:N_STATES + 2])
@@ -72,33 +97,38 @@ class DQN:
         loss.backward()
         self.optimizer.step()
 
+    def generate_state_feature(self, action, score):
+        feature_hot = np.zeros(self.n_questions)
+        feature_hot[action] = score
+        return feature_hot
+
+        # feature_hot = torch.zeros(1, self.n_questions)
+        # feature_hot = feature_hot.scatter_(dim=0, index=torch.from_numpy(action), value=score)
+        # return feature_hot
+
 
 # RL net which generates action(recommended question) at each step
 # to be updated: input question or question_emb ?
-class Net(nn.Module):
-    def __init__(self, emb_file='data/emb_50.txt', ques_h_size=50):
-        super(Net, self).__init__()
+class SimpleNet(nn.Module):
+    def __init__(self,
+                 n_actions = (100, 'number of questions'),
+                 state_feature_size = (50, 'size of state'),
+                 ques_h_size=50):
+        super(SimpleNet, self).__init__()
 
-        self.emb_file = emb_file
-        wcnt, emb_size, words, embs = load_embedding(self.emb_file)
-        self.wcnt = wcnt
-        self.emb_size = emb_size
-        self.words = words
-        self.embs = embs
+        self.n_actions = n_actions
+        self.state_feature_size = state_feature_size
         self.ques_h_size = ques_h_size
 
-        self.input_net = nn.Linear(N_STATES, 200)
-        self.input_net.weight.data.normal_(0, 0.1)
-        self.out_net = nn.Linear(200, N_ACTIONS)
-        self.out_net.weight.data.normal_(0, 0.1)
-
-        self.question_net = QuesNet(self.wcnt, self.emb_size, self.ques_h_size, 1)
+        self.input_net = nn.Linear(self.state_feature_size, 200)
+        self.out_net = nn.Linear(200, self.n_actions)
+        # self.input_net.weight.data.normal_(0, 0.1)
+        # self.out_net.weight.data.normal_(0, 0.1)
 
     def forward(self, x):
-        ques_h0 = None
-        ques_v, ques_h = self.question_net(x.view(-1, 1), ques_h0)
-
-        x = F.relu(self.input_net(ques_v))
-        out = self.out_net(x)
-        return out
+        # input current state: (question, score)
+        # generate next action values (question_)
+        x = F.relu(self.input_net(x))
+        action_values = self.out_net(x)
+        return action_values
 
