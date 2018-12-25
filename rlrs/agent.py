dda0e21599baa23bec30992d6789515748a5132b
huangzhenya <huang3644129@163.com>
2018-12-25 17:23:12 -0600

    agent.py: update simple rnn net, modify select_action(), learn() trainer.py: update train_agent()

--- rlrs/agent.py
+++ rlrs/agent.py
@@ -15,12 +15,11 @@ TARGET_REPLACE_ITER = 100   # target update frequency
 @fret.configurable
 class DQN:
     def __init__(self,
-                 dataset=('zhixue', 'student record dataset',
-                          ['zhixue', 'poj', 'ustcoj']),
                  memory_capacity=(2000, 'max saved memory states'),
                  learning_rate=(0.001, 'learning rate'),
                  greedy_epsilon=(0.9, 'greedy policy'),
                  gama=(0.9, 'reward discount rate'),
+                 n_actions=(100, 'equals to n_questions'),
                  double_q = False,
                  ):
         self.learn_step_counter = 0
@@ -28,40 +27,40 @@ class DQN:
         self.greedy_epsilon = greedy_epsilon
         self.gama = gama
 
-        self.dataset = dataset
-        self._questions = Questions(dataset)
-        self.n_questions = self._questions.n_questions
+        # self.dataset = dataset
+        # self._questions = Questions(dataset)
+        self.n_actions = n_actions
 
         self.memory_counter = 0
         self.memory_capacity = memory_capacity
         self.memory = np.zeros((memory_capacity, 4)) # store state, action, reward, state_
 
-        self._rec_history = []
-        self._pred_scores = []
+        # self._rec_history = []
+        # self._pred_scores = []
 
         # build DQN network
-        self.current_net, self.target_net = SimpleNet(), SimpleNet()
-
-        # self.optimizer = torch.optim.Adam(self.current_net.parameters(), lr=learning_rate)
-        # self.loss_func = nn.MSELoss()
+        self.current_net = SimpleNet(self.n_actions, 50, self.n_actions)
+        self.target_net = SimpleNet(self.n_actions, 50, self.n_actions)
+        self.optimizer = torch.optim.Adam(self.current_net.parameters(), lr=learning_rate)
+        self.loss_func = nn.MSELoss()
 
     def select_action(self, observation):
         # to be updated of input observation
-        [p_score] = observation
-        self._pred_scores.append(p_score)
+        action_ques, p_score = observation
+        # self._pred_scores.append(p_score)
 
         # generate
-        _action = self._rec_history[-1]
-        feature = self.generate_state_feature(_action, p_score)
-        feature = torch.unsqueeze(torch.FloatTensor(feature), 0)
+        # _action = self._rec_history[-1]
+        # feature = self.generate_state_feature(_action, p_score)
+        # feature = torch.unsqueeze(torch.FloatTensor(feature), 0)
 
         if np.random.uniform() < self.greedy_epsilon:
-            action_values = self.current_net(feature)
+            action_values = self.current_net(action_ques, p_score)
             action = torch.max(action_values, 1)[1].data.numpy()
         else:
             action = np.random.randint(0, self.n_questions)
 
-        self._rec_history.append(action)
+        # self._rec_history.append(action)
         return action
 
     def store_transition(self, state, action, reward, state_):
@@ -84,12 +83,12 @@ class DQN:
         b_r = b_memory[: 2]
         b_s_ = b_memory[:, -1]
 
-        b_s_feature = self.generate_state_feature(b_s)
+        # b_s_feature = self.generate_state_feature(b_s)
 
-        b_s = torch.FloatTensor(b_memory[:, 0])
-        b_a = torch.LongTensor(b_memory[:, 1].astype(int))
-        b_r = torch.FloatTensor(b_memory[: 2])
-        b_s_ = torch.FloatTensor(b_memory[:, -1])
+        # b_s = torch.FloatTensor(b_memory[:, 0])
+        # b_a = torch.LongTensor(b_memory[:, 1].astype(int))
+        # b_r = torch.FloatTensor(b_memory[: 2])
+        # b_s_ = torch.FloatTensor(b_memory[:, -1])
 
         q_current = self.current_net(b_s).gather(1, b_a)
         q_next = self.target_net(b_s_).detach()
@@ -101,11 +100,6 @@ class DQN:
         loss.backward()
         self.optimizer.step()
 
-    def generate_state_feature(self, action, score):
-        feature_hot = np.zeros(self.n_questions)
-        feature_hot[action] = score
-        return feature_hot
-
         # feature_hot = torch.zeros(1, self.n_questions)
         # feature_hot = feature_hot.scatter_(dim=0, index=torch.from_numpy(action), value=score)
         # return feature_hot
@@ -115,9 +109,9 @@ class DQN:
 # to be updated: input question or question_emb ?
 class SimpleNet(nn.Module):
     def __init__(self,
-                 n_actions=100,
                  state_feature_size=50,
-                 ques_h_size=50):
+                 ques_h_size=50,
+                 n_actions=100):
         super(SimpleNet, self).__init__()
 
         self.n_actions = n_actions
@@ -129,10 +123,60 @@ class SimpleNet(nn.Module):
         # self.input_net.weight.data.normal_(0, 0.1)
         # self.out_net.weight.data.normal_(0, 0.1)
 
-    def forward(self, x):
+    def forward(self, action, score):
         # input current state: (question, score)
         # generate next action values (question_)
-        x = F.relu(self.input_net(x))
-        action_values = self.out_net(x)
+        # one hot feature from (question, score)
+        feature_x = self.generate_state_feature()
+        feature_x = torch.unsqueeze(torch.FloatTensor(feature_x), 0)
+
+        feature_x = F.relu(self.input_net(feature_x))
+        action_values = self.out_net(feature_x)
         return action_values
+
+    def generate_state_feature(self, action, score):
+        feature_hot = np.zeros(self.n_questions)
+        feature_hot[action] = score
+        return feature_hot
+
+
+# simple RNN net
+class GRUNet(nn.Module):
+    def __init__(self,
+                 state_feature_size=(100, 'input state feature size'),
+                 seq_h_size=(200, 'seq hidden size'),
+                 action_size=(100, 'number of questions'),
+                 n_layers=1):
+        self.state_feature_size = state_feature_size
+        self.seq_h_size = seq_h_size
+        self.action_size = action_size
+        self.n_layers = n_layers
+
+        self.initial_h = nn.Parameter(torch.zeros(n_layers *
+                                                  seq_h_size))
+        self.seq_net = nn.GRU(self.state_feature_size, self.seq_h_size, self.n_layers)
+        self.action_net = nn.Linear(self.seq_h_size, self.action_size)
+
+    def forward(self, action, score, hidden):
+        if hidden is None:
+            h = self.initial_h.view(self.n_layers, 1, self.seq_hidden_size)
+        else:
+            h = hidden
+
+        feature_x = self.generate_state_feature(action, score)
+        feature_x = torch.FloatTensor(feature_x).view(1, 1, -1)
+        _, h = self.seq_net(feature_x, h)
+        action_values = self.action_net(h)
+        return action_values, h
+
+    def generate_state_feature(self, action, score):
+        feature_hot = np.zeros(self.n_questions)
+        feature_hot[action] = score
+        return feature_hot
+
+
+
+
+
+
 
--- rlrs/trainer.py
+++ rlrs/trainer.py
@@ -1,11 +1,79 @@
 import fret
+import numpy as np
+import torch
+import torch.nn as nn
+from .dataprep import Questions
+from .agent import DQN
 
+TARGET_REPLACE_ITER = 100
 
 @fret.configurable
 class Trainer:
-    def __init__(self, env, agent):
+    def __init__(self,
+                 env=(None, 'environment'),
+                 agent=(None, 'RL agent'),
+                 dataset=('zhixue', 'student record dataset',
+                          ['zhixue', 'poj', 'ustcoj']),
+                 ):
         self.env = env
         self.agent = agent
+        self.rec_history = []
+        self.reward_history = []
+        self.pred_score_history = []
+
+        # self.memory = np.zeros()
+
+        self.dataset = dataset
+        self._questions = Questions(dataset)
+        self.n_questions = self._questions.n_questions
+
+    def train_agent(self, args):
+        logger = self.ws.logger('Trainer.train_agent')
+        if self.env is None:
+            logger.info('no environment')
+            exit(0)
+        # self.load_env()
+
+        if self.agent is None:
+            logger.info('no agent loading, build new agent')
+            self.agent = DQN(memory_capacity=500,
+                             learning_rate=0.001,
+                             greedy_epsilon=0.9,
+                             gama=0.9,
+                             n_actions=self.n_questions,
+                             double_q=False)
+        # self.load_agent()
+
+        # RL agent training process here
+        for epoch in range(args.epoch):
+            #TODO: start process: reset env and randomly generate a first recommendation
+            self.rec_history = []
+            self.reward_history = []
+            self.pred_score_history = []
+            state_score = self.env.reset()
+
+            ep_reward = 0
+            while True:
+                action = self.agent.select_action(state_score)
+                # take action in env
+                state_score_, reward, done, info = self.env.step(action)
+
+                # save records
+                self.rec_history.append(action)
+                self.pred_score_history.append(state_score_)
+                self.reward_history.append(reward)
+
+                ep_reward += reward
+
+                # update parameters in agent
+                #TODO: training on batch? and calculate q_current, q_next
+                sample_index = np.random.choice(self.memory_capacity, self.BATCH_SIZE)
+                q_current = 0
+                q_next = 0
+
+    def load_agent(self, path=None):
+        logger = self.ws.logger('Trainer.load_agent')
+        logger.debug('func: <trainer.load_agent>, path=%s', path)
 
     def load_env(self, path=None):
         logger = self.ws.logger('Trainer.load_env')
